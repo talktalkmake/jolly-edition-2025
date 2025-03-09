ba28f0b87e5400c01cb349a27cf83ba362758474
root <root@DESKTOP-IC645JB>
2025-03-09 16:32:24 -0400

    Adding gh pages config

--- vite.config.ts
+++ vite.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "https://jolly-edition-2025.github.io/",
 })
